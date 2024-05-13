194ffac47721cccc79e0c74a830d96fdb0068794
artur0304 <137447718+artur0304@users.noreply.github.com>
2024-05-13 13:39:49 +0300

    Delete lab6 directory

--- lab6/src/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles/fonts.scss";
-import "./styles/gallery.scss";
-import "./styles/gradient.scss";
-import "./styles/nav.scss";
-import "./styles/news.scss";
-import "./styles/normalize.css";
-import "./styles/position.scss";
-import "./styles/table.scss";
